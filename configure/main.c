ddba9b8f62304d0651528c82ab065d666c9c0622
tonyzzp <tonyzzp@qq.com>
2022-06-30 18:05:30 +0800

    configure

---
+++ configure/main.c
@@ -0,0 +1,10 @@
+#include "version.h"
+#include "myname.h"
+#include <stdio.h>
+
+void main()
+{
+    printf("version: %s\n", VERSION);
+    printf("myname: %s\n", MYNAME);
+    printf("wrong_myname: %s\n", WRONG_MYNAME);
+}
